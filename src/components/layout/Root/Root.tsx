d67b5ee97311117d9c2737f29f17897e5250b0ac
Ragn <gost45678@gmail.com>
2024-02-23 19:55:28 +0100

    add footer empty component & add to Root

--- src/components/layout/Root/Root.tsx
+++ src/components/layout/Root/Root.tsx
@@ -1,11 +1,12 @@
-import {Outlet} from 'react-router-dom'
-import {Header} from "components/UI/Header/Header";
+import { Outlet } from 'react-router-dom'
+import { Header } from "components/UI/Header/Header";
+import { Footer } from '../../UI/Footer /Footer';
 export default function Root () {
     return (
         <>
             <Header/>
             <Outlet/>
+            <Footer/>
         </>
-)
-
+    )
 }
