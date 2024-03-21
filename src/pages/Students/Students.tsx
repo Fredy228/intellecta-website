1be8ef9d0cc670b934b17bfeee19801c5d97f38d
Ragn <gost45678@gmail.com>
2024-03-21 20:11:26 +0100

    student teacher admin

--- src/pages/Students/Students.tsx
+++ src/pages/Students/Students.tsx
@@ -1,28 +1,27 @@
 import {SliderStudents} from "components/UI/Slider/StudentSlider/SliderStudent";
 import {IconRobotbook} from "components/Icon/Icons";
 import styles from "./students.module.scss"
-
 export default function StudentsPage() {
     return (
         <main>
             <section className={styles.students}>
                 <div className={"student__container"}>
-                    <div>
-                        <div>
+                    <div className={styles.students__top}>
+                        <div className={styles.students__robot}>
                             <IconRobotbook />
-                            <p>Повна версія | <span>Бета-версія</span></p>
+                            <p>Повна версія <span className={styles.beta}>| Бета-версія</span></p>
                         </div>
                         <SliderStudents />
                     </div>
-                </div>
-                <div>
-                    <ul>
-                        <li>Розклад на тиждень, або відображення усього місяця із домашнім завданням</li>
-                        <li>Посилання на домашні завдання та онлайн уроки</li>
-                        <li>Уся інформація, щодо предмету та викладача</li>
-                        <li>Налаштування іконок для предметів</li>
-                        <li>Блок для нотатків та нагадувань</li>
-                    </ul>
+                    <div>
+                        <ul className={styles.students__menu}>
+                            <li className={styles.students__menu_item}>Розклад на тиждень, або відображення усього місяця із домашнім завданням</li>
+                            <li className={styles.students__menu_item}>Посилання на домашні завдання та онлайн уроки</li>
+                            <li className={styles.students__menu_item}>Уся інформація, щодо предмету та викладача</li>
+                            <li className={styles.students__menu_item}>Налаштування іконок для предметів</li>
+                            <li className={styles.students__menu_item}>Блок для нотатків та нагадувань</li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         </main>
--- src/pages/Teacher/Teacher.tsx
+++ src/pages/Teacher/Teacher.tsx
@@ -1,7 +1,33 @@
+import styles from "../Students/students.module.scss";
+import {IconRobotbook} from "../../components/Icon/Icons";
+import {SliderStudents} from "../../components/UI/Slider/StudentSlider/SliderStudent";
+
 export default function TeacherPage() {
     return(
         <main>
-            <section>Teacher</section>
+            <section className={styles.students}>
+                <div className={"student__container"}>
+                    <div className={styles.students__top}>
+                        <div className={styles.students__robot}>
+                            <IconRobotbook/>
+                            <p>Повна версія <span className={styles.beta}>| Бета-версія</span></p>
+                        </div>
+                        <SliderStudents/>
+                    </div>
+                    <div>
+                        <ul className={styles.students__menu}>
+                            <li className={styles.students__menu_item}>Розклад на тиждень, або відображення усього
+                                місяця із домашнім завданням
+                            </li>
+                            <li className={styles.students__menu_item}>Посилання на домашні завдання та онлайн уроки
+                            </li>
+                            <li className={styles.students__menu_item}>Уся інформація, щодо предмету та викладача</li>
+                            <li className={styles.students__menu_item}>Налаштування іконок для предметів</li>
+                            <li className={styles.students__menu_item}>Блок для нотатків та нагадувань</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </main>
     );
 }
